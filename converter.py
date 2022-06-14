88e26a79b5138b3f3847675d20720880b98da194
rahul-learner <sawrahulrajendra@gmail.com>
2022-06-14 18:02:25 +0530

    completed

--- converter.py
+++ converter.py
@@ -3,15 +3,12 @@ import os
 import colorama
 
 class videoToAscii:
-    def __init__(self, video_path, width=100):
-        self.video_path = video_path
-        self.width = width
+    def __init__(self, video_path, width=100, height=50):
         self.cap = cv2.VideoCapture(video_path)
         self.frame_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.fps = int(self.cap.get(cv2.CAP_PROP_FPS))
-        self.frame_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.frame_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        self.frame_size = (self.frame_width, self.frame_height)
+        self.width = width
+        self.height = height
+        self.frame_size = (self.width, self.height)
 
     def get_frame(self, frame_number):
         self.cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
@@ -22,7 +19,8 @@ class videoToAscii:
         for i in range(0, self.frame_count):
             img_count = str(i)
             frame = self.get_frame(i)
-            frame = cv2.resize(frame, (self.width, int(self.width * self.frame_height / self.frame_width)), interpolation=cv2.INTER_AREA)
+            frame = cv2.resize(frame, self.frame_size)
+            #frame = cv2.resize(frame, (self.width, int(self.width * self.height / self.frame_width)), interpolation=cv2.INTER_AREA)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             # convert image to ascii
             asciiChars = ["@", "#", "S", "%", "&", "*", "+", ";", ":", ",", ".", " "]
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import os
 
 video_path = sys.argv[1]
 
-#get the terminal width
+#get the terminal width and height
+height = os.get_terminal_size().lines
 width = os.get_terminal_size().columns
 # convert video to ascii
-converter.videoToAscii(video_path, width).convert()
+converter.videoToAscii(video_path, width, height).convert()
