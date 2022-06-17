4acac5acba4ed7c011bb09e5981b12e881f485a1
rahul-learner <sawrahulrajendra@gmail.com>
2022-06-17 17:55:41 +0530

    in progress but ready to run

--- app.py
+++ app.py
@@ -8,18 +8,18 @@ from ffpyplayer.player import MediaPlayer
 video_path = sys.argv[1]
 
 def ascii_image(image):
-    image = image
-    img = image.resize((os.get_terminal_size().columns, os.get_terminal_size().lines))
-
-    img = img.convert('L')
-    pixels = img.getdata()
+    image = image.convert('L')
+    pixels = image.getdata()
 
     # replace each pixel with a chartacter from array
     chars = " _.,-=+:;!?0123456789$W#@N" #" _.,-=+:;cba!?0123456789$W#@N" #['@', '#', '$', '%', '^', '&', '*', ':', ';', '.', ',']
     new_pixels = [chars[pixel//25] for pixel in pixels]
     new_pixels = ''.join(new_pixels)
 
-    print(new_pixels, end="\r")
+    print("\r" + new_pixels, end="")
+
+
+
 
 player = MediaPlayer(video_path)
 vid = cv2.VideoCapture(video_path)
@@ -27,14 +27,27 @@ fps = int(vid.get(cv2.CAP_PROP_FPS))
 
 while True:
     _, frame = vid.read()
-    audio_frame, val = player.get_frame()
-    now = time.time()
+    if _:
+        audio_frame, val = player.get_frame()
+        now = time.time()
 
-    #Do your thing
-    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-    frame = Image.fromarray(frame)
-    ascii_image(frame)
+        #Do your thing
+        #frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        frame = Image.fromarray(frame)
+        # terminla size of both windows and linux
+        width, height = os.get_terminal_size()
+        #
+        frame = frame.resize((os.get_terminal_size().columns, os.get_terminal_size().lines))
+        ascii_image(frame)
 
-    timeDiff = time.time() - now
-    if (timeDiff < 1.0/(fps)):
-        time.sleep(1.0/(fps) - timeDiff)
+        timeDiff = time.time() - now
+        if (timeDiff < 1.0/(fps)):
+            time.sleep(1.0/(fps) - timeDiff)
+
+    else:
+        break
+
+player.close()
+cv2.release()
+cv2.destroyAllWindows()
+sys.exit(0)
--- main.py
+++ main.py
@@ -1,70 +1,41 @@
-import cv2
 from PIL import Image
-import os
-import sys
+import cv2
 import time
+import sys
+import os
 from ffpyplayer.player import MediaPlayer
-
-#asciiChars = " _.,-=+:;cba!?0123456789$W#@N" " .,:;+*&%$#@" ["@", "#", "S", "%", "&", "*", "+", ";", ":", ",", ".", " "]
 
 video_path = sys.argv[1]
 
 def ascii_image(image):
-    image = image
-    img = image.resize((os.get_terminal_size().columns, os.get_terminal_size().lines))
-
-    img = img.convert('L')
-    pixels = img.getdata()
+    image = image.convert('L')
+    pixels = image.getdata()
 
     # replace each pixel with a chartacter from array
     chars = " _.,-=+:;!?0123456789$W#@N" #" _.,-=+:;cba!?0123456789$W#@N" #['@', '#', '$', '%', '^', '&', '*', ':', ';', '.', ',']
     new_pixels = [chars[pixel//25] for pixel in pixels]
     new_pixels = ''.join(new_pixels)
 
-    print(new_pixels, end="\n")
+    print(new_pixels, end="\r")
 
 
 
-cap = cv2.VideoCapture(video_path)
+
 player = MediaPlayer(video_path)
-fps = int(cap.get(cv2.CAP_PROP_FPS))
-try:
-    start_time = time.time()
-    start = 0
-    end = 0
-    while(True):
-        start = time.time()
-        ret, frame = cap.read()
-        audio_frame, val = player.get_frame()
-        if ret != True:
-            break
-        else:
-            frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-            frame = Image.fromarray(frame)
-            ascii_image(frame)
-            time_taken = time.time() - start
-            sleep_time =(1 / fps - time_taken)
-            if sleep_time > 0:
-                time.sleep(sleep_time)
-                print("Sleeping for: " + str(sleep_time))
-            else:
-                print("Wake for: " + str(sleep_time))
+vid = cv2.VideoCapture(video_path)
+fps = int(vid.get(cv2.CAP_PROP_FPS))
 
-            end = time.time()
-            frps = 1 / (end - start)
-            start = end
+while True:
+    _, frame = vid.read()
+    audio_frame, val = player.get_frame()
+    now = time.time()
 
-            frps = int(frps)
-            print("FPS: " + str(frps))
-            
+    #Do your thing
+    #frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    frame = Image.fromarray(frame)
+    frame = frame.resize((os.get_terminal_size().columns, os.get_terminal_size().lines))
+    ascii_image(frame)
 
-    cap.release()
-    #print the time it take to run the program
-    current_time = (time.time() - start_time)
-    print("Time taken to run the program: " + str(current_time / 60) + " minutes")
-    #cv2.destroyAllWindows()
-
-except KeyboardInterrupt:
-    cap.release()
-    cv2.destroyAllWindows()
-
+    timeDiff = time.time() - now
+    if (timeDiff < 1.0/(fps)):
+        time.sleep(1.0/(fps) - timeDiff)
